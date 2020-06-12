7dfd9a87aa50f5491bed4603f8bdedc98cb26fc3
Michael Gunn <mgunn@ucsc.edu>
2020-06-12 16:57:25 -0700

    Click to place bodies added, no stable configurations though

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### Gravitation
 
 This is my implementation of gravitation and the three body problem using P5.js. The heart of the physics is Newton's Law of Universal Gravitation, F = G*m1*m2/r^2. 
-The reset button reinitializes the three bodies at random positions with random initial velocities. The stable button resets to a stable configuration which forms a figure eight first discovered in 1993.
+The reset button reinitializes the three bodies at random positions with random initial velocities. The "Pause" button clears the simulation and allows you to place bodies by clicking in the window with the mouse to create a body at the cursor. The simulation can be resumed once bodis are placed by pressing "Resume"
 
 
 To view simply download the project and open index.html
--- sketch.js
+++ sketch.js
@@ -3,53 +3,43 @@
 
 let bodies = [];
 let colors = [];
-let radius = 40;
+let radius = 30;
 let speed = 0;
 let G = 500;
-let resetButton, stableButton;
+let resetButton, pauseButton, resumeButton;
 
 function setup() {
     createCanvas(800, 640);
     background(0);
-    // let b1 = new body(100, 300, random(speed), random(speed));
-    // let b2 = new body(500, 200, random(speed), random(speed));
-    // let b3 = new body(500, 500, random(speed), random(speed));
     let b1 = new body(random(width * 0.2, width * 0.8), random(height * 0.2, height * 0.8), random(speed), random(speed));
     let b2 = new body(random(width * 0.2, width * 0.8), random(height * 0.2, height * 0.8), random(speed), random(speed));
     let b3 = new body(random(width * 0.2, width * 0.8), random(height * 0.2, height * 0.8), random(speed), random(speed));
-    // bodies.push(b1);
-    // bodies.push(b2);
-    // bodies.push(b3);
 
-    // let scale = 100;
-    // let b1 = new body(-1 * scale, 0 * scale, 0.347111 * scale / 10, 0.532728 * scale / 10);
-    // let b2 = new body(1 * scale, 0 * scale, 0.347111 * scale / 10, 0.532728 * scale / 10);
-    // let b3 = new body(0 * scale, 0 * scale, -2 * 0.347111 * scale / 10, -2 * 0.532728 * scale / 10);
-    // let p1 = 0.347111;
-    // let p2 = 0.532728;
-    // let b1 = new body(-10, 0, p1, p2);
-    // let b2 = new body(10, 0, p1, p2);
-    // let b3 = new body(0, 0, -2 * p1, -2 * p2);
+
     bodies = [];
     bodies.push(b1);
     bodies.push(b2);
     bodies.push(b3);
-    // translate(width / 2, height / 2);
-    // scale(50);
 
     resetButton = createButton('Reset');
     resetButton.mousePressed(reset);
     resetButton.position(30, height + 20);
-    stableButton = createButton('Stable');
-    stableButton.mousePressed(stable);
-    stableButton.position(30, height + 50);
+
+    pauseButton = createButton('Pause');
+    pauseButton.mousePressed(pause);
+    pauseButton.position(30, height + 50);
+
+    resumeButton = createButton('Resume');
+    resumeButton.mousePressed(resume);
+    resumeButton.position(30, height + 80);
 
     let c;
     for (let i = 0; i < bodies.length; i++) {
         let r = random(255);
         let g = random(255);
         let b = random(255);
-        c = color(r, g, b);
+        let a = random(200, 255);
+        c = color(r, g, b, a);
         colors.push(c);
     }
 }
@@ -57,9 +47,6 @@ function setup() {
 function draw() {
     background(0, 3);
     noStroke();
-    //walls 
-    // translate(width / 2, height / 2);
-    // scale(20);
 
     let i = 0;
     for (let body of bodies) {
@@ -137,15 +124,30 @@ function reset() {
 
 }
 
-function stable() {
+
+function pause() {
+    noLoop();
     background(0);
-    let scale = 20;
-    let b1 = new body(-1 * scale, 0 * scale, 0.347111 * scale, 0.532728 * scale);
-    let b2 = new body(1 * scale, 0 * scale, 0.347111 * scale, 0.532728 * scale);
-    let b3 = new body(0 * scale, 0 * scale, -2 * 0.347111 * scale, -2 * 0.532728 * scale);
     bodies = [];
-    bodies.push(b1);
-    bodies.push(b2);
-    bodies.push(b3);
-    translate(width / 2, height / 2);
+
+}
+
+function resume() {
+    loop();
+}
+
+function mousePressed() {
+    let x = mouseX;
+    let y = mouseY;
+    if (x >= 0 && x <= width && y >= 0 && y <= height) { //if cursor is within the window
+        let b = new body(x, y, random(speed), random(speed));
+        bodies.push(b);
+    }
+    let r = random(255); //add a new random color to the colors array to prevent problems if more than 3 bodies are added
+    let g = random(255);
+    let b = random(255);
+    let c = color(r, g, b);
+    colors.push(c);
+    colors.push(c)
+    redraw();
 }
